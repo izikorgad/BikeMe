1b45f6f1baa090f4ec83240ace8c774eb9f14c19
izik <izikorgad@gmail.com>
2018-06-21 14:56:49 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import { Tabs } from './app/config/router';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Tabs />
+    );
+  }
+}
---
+++ app/screens/PageViewScreen.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { View, TouchableHighlight } from 'react-native';
+import SwiperScreen from '../components/SwiperScreen';
+import { Header } from '../components/common';
+
+export default class PageViewScreen extends Component {
+  render() {
+    return (
+      <View style={{
+        flex: 1
+      }}>
+        <Header headerText='Page View Screen' />
+        <SwiperScreen />
+      </View>
+    );
+  }
+}
